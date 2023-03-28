b0b3dd7362dc34ec6fce05e475bd5614dc3a990e
Mustafa Melih Yumak <muyumak@k1m33s02.42istanbul.com.tr>
2023-03-28 03:31:54 +0300

    exec files

---
+++ minishell/exec_cd.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec_cd.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muyumak <muyumak@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/28 02:29:01 by muyumak           #+#    #+#             */
+/*   Updated: 2023/03/28 03:27:36 by muyumak          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	go_back(char *env_pwd)
+{
+	int	i;
+
+	i = ft_strlen(env_pwd) - 1;
+	while (env_pwd[i] && env_pwd[i] != '/')
+	{
+		env_pwd[i] = 0;
+		i--;
+	}
+	env_pwd[i] = 0;
+	chdir(env_pwd);
+}
+
+void	exec_cd(void)
+{
+	static char	*env_pwd;
+	
+	printf("%s\n", env_pwd);
+	if (!env_pwd)
+		env_pwd = getenv("PWD");
+	if (g_arg.args[1] == 0)
+	{
+		env_pwd = getenv("HOME");
+		chdir(getenv("HOME"));
+	}
+	else if (!ft_strncmp("..", g_arg.args[1], 2))
+		go_back(env_pwd);
+	else
+	{
+		env_pwd = ft_strjoin(env_pwd, "/");
+		env_pwd = ft_strjoin(env_pwd, g_arg.args[1]);
+		chdir(env_pwd);
+	}
+	printf("%s\n", env_pwd);
+}
